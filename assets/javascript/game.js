e25c30b6887ce52e667f2dbb1dffb0fb79b87561
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-02-11 20:35:51 -0800

    add guess count and failed attempt letters, clears after certain amount of tries

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,18 +12,30 @@ var wordsArray = [
 var randomIndexOfArray = Math.floor(Math.random() * wordsArray.length);
 var randomWord = wordsArray[randomIndexOfArray];
 var allLettersGuessed = [];
+const maxAttempts = 15;
+var guessCount = 0;
+var guessesRemaining = maxAttempts - guessCount;
 
 document.onkeydown = function(e) {
 	var theKey = e.key.toLowerCase();
 	var theKeyCode = e.keyCode;
 
-	if(theKeyCode >= 65 && theKeyCode <= 90 && allLettersGuessed.indexOf(theKey) === -1){
+	if(theKeyCode >= 65 && theKeyCode <= 90 && allLettersGuessed.indexOf(theKey) === -1 && guessesRemaining > 0){
 		allLettersGuessed.push(theKey);
 		var html = "";
 		for(var i = 0; i < allLettersGuessed.length; i++) {
 			html += allLettersGuessed[i].toUpperCase();
 		}
+		guessCountElement.innerHTML = guessesRemaining;
+		guessCount++;
+		guessesRemaining = maxAttempts - guessCount;
 		lettersGuessedElement.innerHTML = html;
 		console.log(allLettersGuessed);
+	} else if(guessesRemaining === 0) {
+		guessCountElement.innerHTML = maxAttempts;
+		guessCount = 0;
+		guessesRemaining = maxAttempts - guessCount;
+		allLettersGuessed = [];
+		lettersGuessedElement.innerHTML = "";
 	}
 }
