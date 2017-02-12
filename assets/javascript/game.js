5c95207df40d93bd872e093f44a67a93e58eb6cd
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-02-11 21:21:49 -0800

    finish reset after no more remaining guesses and rendering words properly (only displaying when guessed), and finish display of failed letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,32 +10,53 @@ var wordsArray = [
 ];
 
 var randomIndexOfArray = Math.floor(Math.random() * wordsArray.length);
-var randomWord = wordsArray[randomIndexOfArray];
+var randomWord = wordsArray[randomIndexOfArray].toLowerCase();
 var allLettersGuessed = [];
 const maxAttempts = 15;
 var guessCount = 0;
 var guessesRemaining = maxAttempts - guessCount;
 
+function renderWord() {
+	var html = "";
+	for(var i = 0; i < randomWord.length; i++) {
+		if(allLettersGuessed.indexOf(randomWord[i]) !== -1) {
+			html += randomWord[i].toUpperCase();
+		} else {
+			html += "_";
+		}
+	}
+	currentWordElement.innerHTML = html;
+}
+
+renderWord();
+
 document.onkeydown = function(e) {
 	var theKey = e.key.toLowerCase();
 	var theKeyCode = e.keyCode;
 
-	if(theKeyCode >= 65 && theKeyCode <= 90 && allLettersGuessed.indexOf(theKey) === -1 && guessesRemaining > 0){
+	if(theKeyCode >= 65 && theKeyCode <= 90 && allLettersGuessed.indexOf(theKey) === -1){
 		allLettersGuessed.push(theKey);
+		if(randomWord.indexOf(theKey) === -1) {
+			guessCount++;
+		}
+		guessesRemaining = maxAttempts - guessCount;
+		if(guessesRemaining === 0) {
+			guessCountElement.innerHTML = maxAttempts;
+			guessCount = 0;
+			guessesRemaining = maxAttempts - guessCount;
+			allLettersGuessed = [];
+			lettersGuessedElement.innerHTML = "";
+			randomWord = wordsArray[randomIndexOfArray];
+		} else {
+			guessCountElement.innerHTML = guessesRemaining;
+		}
 		var html = "";
 		for(var i = 0; i < allLettersGuessed.length; i++) {
-			html += allLettersGuessed[i].toUpperCase();
+			if(randomWord.indexOf(allLettersGuessed[i]) === -1) {
+				html += allLettersGuessed[i].toUpperCase();
+			}
 		}
-		guessCountElement.innerHTML = guessesRemaining;
-		guessCount++;
-		guessesRemaining = maxAttempts - guessCount;
 		lettersGuessedElement.innerHTML = html;
-		console.log(allLettersGuessed);
-	} else if(guessesRemaining === 0) {
-		guessCountElement.innerHTML = maxAttempts;
-		guessCount = 0;
-		guessesRemaining = maxAttempts - guessCount;
-		allLettersGuessed = [];
-		lettersGuessedElement.innerHTML = "";
-	}
+		renderWord();
+	} 
 }
